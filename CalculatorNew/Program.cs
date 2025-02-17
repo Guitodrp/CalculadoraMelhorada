f668a6c2968ce17c1767c8c91b4347fb8e7a3ec7
Yago Oliveira <guito_flash@yahoo.com>
2025-02-17 14:10:14 -0300

    Add project files.

---
+++ CalculatorNew/Program.cs
@@ -0,0 +1,77 @@
+﻿CalculatorNew calculatorNew = new("Calculadora!");
+calculatorNew.Run();
+
+public sealed class CalculatorNew(string message)
+{
+    private string _message = message;
+
+    public void Run()
+    {
+        Console.WriteLine(_message + "\n");
+
+        Dictionary<int, string> operadores = new()
+        {
+            { 1, "+ Soma" },
+            { 2, "- Subtração" },
+            { 3, "* Multiplicação" },
+            { 4, "/ Divisão" }
+        };
+
+        while (true)
+        {
+            Console.WriteLine("Operadores: ");
+            foreach (var op in operadores)
+            {
+                Console.WriteLine($"{op.Key}: {op.Value}");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Escolha um operador: ");
+            int operador = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            if (!operadores.TryGetValue(operador, out string operadorTexto))
+            {
+                Console.WriteLine("Operador inválido!");
+                continue;
+            }
+            Console.WriteLine("Digite o primeiro número: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Resultado: " + Calcular(operador, num1, num2));
+            Console.WriteLine();
+
+            //Console.WriteLine("Deseja realizar outra conta? (s/n)");
+
+            Console.Write("Press <Enter> to continue... ");
+            if(Console.ReadKey().Key != ConsoleKey.Enter) 
+            {
+                break;
+            }
+        }
+    }
+    public static double Calcular(int operador, double num1, double num2)
+    {
+        double resultado = 0;
+        switch (operador)
+        {
+            case 1:
+                resultado = num1 + num2;
+                break;
+            case 2:
+                resultado = num1 - num2;
+                break;
+            case 3:
+                resultado = num1 * num2;
+                break;
+            case 4:
+                resultado = num1 / num2;
+                break;
+        }
+        return resultado;
+    }
+
+}
